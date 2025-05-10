9fd2efea210d1a057cc125bcb7cbd1c044e3fa4c
Sahar Zare <79926856+saharzare@users.noreply.github.com>
2025-05-10 22:45:28 +0330

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,1 @@
+Scripts Purpose: This Scripts crete a new database as a DataWareHouse, after checking if there already not exists. If it exists it drops and recreats. and has the setups for creating 3 schemas
