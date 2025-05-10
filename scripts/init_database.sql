fe1eaa972db5083571716294c50288b5e5752ed1
Sahar Zare <79926856+saharzare@users.noreply.github.com>
2025-05-10 22:48:21 +0330

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,23 @@
-Scripts Purpose: This Scripts crete a new database as a DataWareHouse, after checking if there already not exists. If it exists it drops and recreats. and has the setups for creating 3 schemas
+/*
+Scripts Purpose: This Scripts crete a new database as a DataWareHouse, after checking if there already not exists. If it exists it drops and recreats. and has the setups for creating 3 schemas.
+Warning: Running this script will drop the entire 'DataWarehouse' database if it exists. make sure there is backup.
+
+
+*/
+
+-- drop if the datawarehouse already exists
+  DROP DATABASE DataWarehouse
+-- create databases
+CREATE DATABASE DataWareHouse;
+go
+
+USE DataWareHouse;
+go
+
+-- create schema
+CREATE SCHEMA bronze
+go
+CREATE SCHEMA silver
+go
+CREATE SCHEMA gold
+go
