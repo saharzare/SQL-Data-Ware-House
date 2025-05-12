99a364904b235b4e0fb1212e926a55fb2f3e7944
Sahar Zare <79926856+saharzare@users.noreply.github.com>
2025-05-13 02:28:35 +0330

    Update load.sql

--- scripts/bronze/load.sql
+++ scripts/bronze/load.sql
@@ -13,9 +13,11 @@ Parameters:
 	  This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    EXEC bronze.load_bronze;
+   EXEC bronze.load_bronze
+  
 ===============================================================================
 */
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
@@ -31,47 +33,51 @@ BEGIN
 
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.crm_cust_info';
+
 		TRUNCATE TABLE bronze.crm_cust_info;
+		
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\sql\dwh_project\datasets\source_crm\cust_info.csv'
+		FROM 'E:\DataEngineer\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SET @end_time = GETDATE();
+		firstrow = 2,
+		fieldterminator = ',',
+		tablock);
+
+SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -------------';
 
         SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.crm_prd_info';
-		TRUNCATE TABLE bronze.crm_prd_info;
+		TRUNCATE TABLE [bronze].[crm_prd_info];
+				PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 
-		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
-		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\sql\dwh_project\datasets\source_crm\prd_info.csv'
+		BULK INSERT [bronze].[crm_prd_info]
+		FROM 'E:\DataEngineer\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK)
+
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -------------';
 
         SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.crm_sales_details';
-		TRUNCATE TABLE bronze.crm_sales_details;
+
+		TRUNCATE TABLE [bronze].[crm_sales_details];
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
-		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\sql\dwh_project\datasets\source_crm\sales_details.csv'
+
+		BULK INSERT [bronze].[crm_sales_details]
+		FROM 'E:\DataEngineer\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SET @end_time = GETDATE();
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK)
+SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -------------';
 
@@ -81,45 +87,49 @@ BEGIN
 		
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.erp_loc_a101';
-		TRUNCATE TABLE bronze.erp_loc_a101;
+		TRUNCATE TABLE [bronze].[erp_cust_az12];
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
-		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\sql\dwh_project\datasets\source_erp\loc_a101.csv'
+
+		BULK INSERT [bronze].[erp_cust_az12]
+		FROM 'E:\DataEngineer\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK);
+
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -------------';
 
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.erp_cust_az12';
-		TRUNCATE TABLE bronze.erp_cust_az12;
+
+		TRUNCATE TABLE [bronze].[erp_loc_a101];
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
-		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\sql\dwh_project\datasets\source_erp\cust_az12.csv'
+
+		BULK INSERT [bronze].[erp_loc_a101]
+		FROM 'E:\DataEngineer\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK)
+
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -------------';
 
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
-		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+		TRUNCATE TABLE [bronze].[erp_px_cat_g1v2];
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
-		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\sql\dwh_project\datasets\source_erp\px_cat_g1v2.csv'
+
+		BULK INSERT [bronze].[erp_px_cat_g1v2]
+		FROM 'E:\DataEngineer\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK);
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -------------';
@@ -138,4 +148,4 @@ BEGIN
 		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
 		PRINT '=========================================='
 	END CATCH
-END
+end
